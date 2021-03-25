907a895969e214c9528ab3f61890d01014bd3d64
liyang <liyang.hlw@vivo.com>
2021-03-25 15:27:22 +0800

    增加当前日期的间隔days的日期

--- basic/src/main/java/com/ly/date/CalculateDay.java
+++ basic/src/main/java/com/ly/date/CalculateDay.java
@@ -9,17 +9,36 @@ public class CalculateDay {
 
     public static void main(String[] args) throws ParseException {
 
-        DateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = dateFormat1.parse("2021-03-19");
+        /**
+         * 测试两个时间相距几天
+         */
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = df.parse("2021-03-19");
         System.out.println(date);
 
         System.out.println("现在与给定时间相距多少天: " + differentDays(date));
+
+
+        /**
+         * 测试某个时间几天前是什么日期
+         */
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date currentDay = new Date();
+        System.out.println("当前时间：      " + sdf.format(currentDay));
+        System.out.println("当前时间的3天前：" + sdf.format(currentDayBefore(3)));
+
     }
 
 
     public static int differentDays(Date date) {
         int days = (int) ((System.currentTimeMillis() - date.getTime()) / (1000 * 3600 * 24));
         return days;
+    }
+
+    public static Date currentDayBefore(int days) {
+        long milliseconds = System.currentTimeMillis() - 1000 * 3600 * 24 * days;
+        Date date = new Date(milliseconds);
+        return date;
     }
 
 }
